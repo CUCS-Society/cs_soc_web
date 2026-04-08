412043b60b52d501d4ae67fdca4742caa9c2b9fd
Matthew Chor <matthewchor9810@gmail.com>
2026-04-08 19:17:30 +0800

    use prisma for better-auth

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -1,8 +1,11 @@
 import { betterAuth } from "better-auth";
-import Database from "better-sqlite3";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "../prisma";
 
 export const auth = betterAuth({
-    database: new Database("sqlite.db"),
+    database: prismaAdapter(prisma, {
+        provider: "sqlite",
+    }),
     baseURL: process.env.BETTER_AUTH_BASE_URL || "http://localhost:3000/~cnchor4",
     emailAndPassword: {
         enabled: true,
